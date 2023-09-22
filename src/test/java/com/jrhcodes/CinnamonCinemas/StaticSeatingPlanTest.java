09723b0868caabf719b7eceb997ae75324e719fc
Julian <mrjrhirst@gmail.com>
2023-09-22 23:40:48 +0100

    testStaticSeatingPlanConstructor refactored into testGetLabel,  testGetFirstRow, testGetFirstSeatNumber, testGetNumberOfSeatsPerRow, testGetNumberOfRows

--- src/test/java/com/jrhcodes/CinnamonCinemas/StaticSeatingPlanTest.java
+++ src/test/java/com/jrhcodes/CinnamonCinemas/StaticSeatingPlanTest.java
@@ -6,14 +6,37 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StaticSeatingPlanTest {
+    @ParameterizedTest
+    @CsvFileSource(resources = "testStaticSeatingPlanConstructor.csv", numLinesToSkip = 1)
+    public void testGetLabel(String label, char firstRowLetter, int firstSeatNumber, int numberOfRows, int seatsPerRow  ) {
+        StaticSeatingPlan plan = new StaticSeatingPlan(label, firstRowLetter, firstSeatNumber, numberOfRows, seatsPerRow);
+        assertEquals(plan.getLabel(), label);
+    }
 
     @ParameterizedTest
     @CsvFileSource(resources = "testStaticSeatingPlanConstructor.csv", numLinesToSkip = 1)
-    public void testStaticSeatingPlanConstructor(String label, char firstRowLetter, int firstSeatNumber, int numberOfRows, int seatsPerRow  ) {
+    public void testGetFirstRow(String label, char firstRowLetter, int firstSeatNumber, int numberOfRows, int seatsPerRow  ) {
+        StaticSeatingPlan plan = new StaticSeatingPlan(label, firstRowLetter, firstSeatNumber, numberOfRows, seatsPerRow);
+        assertEquals(plan.getFirstRowLetter(), firstRowLetter);
+    }
+    @ParameterizedTest
+    @CsvFileSource(resources = "testStaticSeatingPlanConstructor.csv", numLinesToSkip = 1)
+    public void testGetFirstSeatNumber(String label, char firstRowLetter, int firstSeatNumber, int numberOfRows, int seatsPerRow  ) {
         StaticSeatingPlan plan = new StaticSeatingPlan(label, firstRowLetter, firstSeatNumber, numberOfRows, seatsPerRow);
         assertEquals(plan.getFirstSeatNumber(), firstSeatNumber);
-        assertEquals(plan.getLabel(), label);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "testStaticSeatingPlanConstructor.csv", numLinesToSkip = 1)
+    public void testGetNumberOfSeatsPerRow(String label, char firstRowLetter, int firstSeatNumber, int numberOfRows, int seatsPerRow  ) {
+        StaticSeatingPlan plan = new StaticSeatingPlan(label, firstRowLetter, firstSeatNumber, numberOfRows, seatsPerRow);
         assertEquals(plan.getNumberOfSeatsPerRow(), seatsPerRow);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "testStaticSeatingPlanConstructor.csv", numLinesToSkip = 1)
+    public void testGetNumberOfRows(String label, char firstRowLetter, int firstSeatNumber, int numberOfRows, int seatsPerRow  ) {
+        StaticSeatingPlan plan = new StaticSeatingPlan(label, firstRowLetter, firstSeatNumber, numberOfRows, seatsPerRow);
         assertEquals(plan.getNumberOfRows(), numberOfRows);
     }
     @ParameterizedTest
